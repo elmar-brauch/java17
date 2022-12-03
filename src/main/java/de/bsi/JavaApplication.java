ea493e5b609e09395a4985bf525682598de792bc
Elmar Brauch <elmar.brauch@telekom.de>
2022-12-03 21:17:44 +0100

    Spring Update & Java 19 update

--- src/main/java/de/bsi/JavaApplication.java
+++ src/main/java/de/bsi/JavaApplication.java
@@ -1,7 +1,5 @@
 package de.bsi;
 
-import javax.annotation.PreDestroy;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/de/bsi/java17/Java17Item.java
+++ src/main/java/de/bsi/java17/Java17Item.java
@@ -1,4 +1,7 @@
 package de.bsi.java17;
 
+import lombok.Builder;
+
 // JEP 395: Records
+@Builder
 public record Java17Item(String name, int id) {}
